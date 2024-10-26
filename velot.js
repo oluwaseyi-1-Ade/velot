88519756a7ff2ba6e630c66d0191012e258eca6d
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2024-10-26 12:45:31 +0100

    removed navLink icon

--- velot.js
+++ velot.js
@@ -1,9 +1,15 @@
-let navBar = document.getElementById("navbar")
-let navLinks = document.getElementsByClassName("nav-links");
-
+const navBar = document.getElementById("navbar");
+const navLinks = document.getElementsByClassName("nav-links");
 
 navBarClick = () => {
-   navLinks[0].classList.add("small-nav")
-   }
+  navLinks[0].setAttribute();
+  if (navLinks[0].classList.contains("hidden")) {
+    navLinks[0].classList.remove("hidden");
+  } else {
+    navLinks[0].classList.add("hidden");
+  }
+};
 
-navBar.addEventListener("click", navBarClick)
+navBar.addEventListener("click", navBarClick);
+let age; //declaration
+let age1 = 20; //initialization
