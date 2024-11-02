aad3d1c0903e761597fe3b5e8b07f957f494dc5a
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2024-11-02 12:59:29 +0100

    updates

--- velot.js
+++ velot.js
@@ -1,15 +1,17 @@
 const navBar = document.getElementById("navbar");
 const navLinks = document.getElementsByClassName("nav-links");
+const link = document.getElementsByTagName("li a");
 
 navBarClick = () => {
-  navLinks[0].setAttribute();
   if (navLinks[0].classList.contains("hidden")) {
     navLinks[0].classList.remove("hidden");
+    navLinks[0].classList.add("active");
   } else {
     navLinks[0].classList.add("hidden");
   }
 };
 
 navBar.addEventListener("click", navBarClick);
-let age; //declaration
-let age1 = 20; //initialization
+
+// let age; //declaration
+// let age1 = 20; //initialization
