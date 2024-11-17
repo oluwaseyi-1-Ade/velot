c305c8b9dc0381768667599cfdc1bc3ffccc1760
oluwaseyi-1-Ade <154804947+oluwaseyi-1-Ade@users.noreply.github.com>
2024-11-17 17:47:45 +0100

    updates

--- velot.js
+++ velot.js
@@ -1,15 +1,17 @@
 const navButton = document.getElementById("nav-btn");
-const navLinks = document.getElementsByClassName("nav-links");
-const link = document.getElementsByTagName("li a");
+const dropdownLink = document.querySelectorAll("#dropdown-links li");
 let dropdown = document.getElementById("dropdown");
 
 navButtonClick = () => {
-  dropdown.classList.toggle("active");
-  dropdown.classList.remove(" mt-[-400px]");
-  console.log("ade");
+  dropdown.classList.toggle("translate-y-[150px]");
 };
+navButton.onclick = navButtonClick;
 
-navButton.addEventListener("click", navButtonClick);
+for (let i = 0; i <= 5; i++) {
+  dropdownLink[i].onclick = () => {
+    dropdown.classList.remove("translate-y-[150px]");
+  }
+}
 
 // let age; //declaration
 // let age1 = 20; //initialization
